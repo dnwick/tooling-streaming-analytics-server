2beccb9329923aa6c8923ea07809250845138ffb
Damith <damith@damith-Latitude-E6540>
2016-11-25 10:47:52 +0530

    added initial templating

--- modules/siddhi-editor/js/main.js
+++ modules/siddhi-editor/js/main.js
@@ -16,11 +16,13 @@
  * under the License.
  */
 
-define(['require', 'log', 'jquery', 'lodash', 'backbone', 'breadcrumbs', 'file_browser', 'tab/service-tab-list', 'app/tool-palette/tool-palette', 'event',
+alert(5555);
+
+define(['require', 'log', 'jquery', 'lodash', 'backbone', 'commons_app/js/menu-bar/menu-bar',
 
     /* void modules */ 'jquery_ui', 'bootstrap'],
 
-    function (require, log, $, _, Backbone, BreadcrumbController, FileBrowser, TabController, kToolPalette, Event) {
+    function (require, log, $, _, Backbone, MenuBar) {
 
     var Application = Backbone.View.extend(
     /** @lends Application.prototype */
@@ -32,69 +34,78 @@ define(['require', 'log', 'jquery', 'lodash', 'backbone', 'breadcrumbs', 'file_b
          * @param {Object} config configuration options for the application
          */
         initialize: function (config) {
-            this.validateConfig(config);
+            //this.validateConfig(config);
             this.config = config;
             this.initComponents();
         },
 
         initComponents: function(){
             // init breadcrumbs controller
-            this.breadcrumbController = new BreadcrumbController(_.get(this.config, "breadcrumbs"));
+            //this.breadcrumbController = new BreadcrumbController(_.get(this.config, "breadcrumbs"));
 
             //init file browser
-            var fileBrowserOpts = _.get(this.config, "file_browser");
-            _.set(fileBrowserOpts, 'application', this);
-            this.fileBrowser = new FileBrowser(fileBrowserOpts);
+//            var fileBrowserOpts = _.get(this.config, "file_browser");
+//            _.set(fileBrowserOpts, 'application', this);
+//            this.fileBrowser = new FileBrowser(fileBrowserOpts);
 
             //init tool palette
-            var toolPaletteOpts = _.get(this.config, "tab_controller.tool_palette");
-            _.set(toolPaletteOpts, 'application', this);
-            this.toolPalette = new ToolPalette(toolPaletteOpts);
+//            var toolPaletteOpts = _.get(this.config, "tab_controller.tool_palette");
+//            _.set(toolPaletteOpts, 'application', this);
+//            this.toolPalette = new ToolPalette(toolPaletteOpts);
 
             //init tab controller
-            var tabControlOpts = _.get(this.config, "tab_controller");
-            _.set(tabControlOpts, 'application', this);
-            // tab controller will take care of rendering tool palette
-            _.set(tabControlOpts, 'toolPalette', this.toolPalette);
-            this.eventManager = new Event();
-            this.tabController = new TabController(tabControlOpts);
+//            var tabControlOpts = _.get(this.config, "tab_controller");
+//            _.set(tabControlOpts, 'application', this);
+//            // tab controller will take care of rendering tool palette
+//            _.set(tabControlOpts, 'toolPalette', this.toolPalette);
+//            this.eventManager = new Event();
+//            this.tabController = new TabController(tabControlOpts);
+
+            //init menu bar
+            var menuBarOpts = _.get(this.config, "menu_bar");
+            _.set(menuBarOpts, 'application', this);
+            this.menuBar = new MenuBar(menuBarOpts);
         },
 
-        validateConfig: function(config){
-            if(!_.has(config, 'services.workspace.endpoint')){
-                throw 'config services.workspace.endpoint could not be found for remote log initialization.'
-            } else {
-                log.initAjaxAppender(_.get(config, 'services.workspace.endpoint'));
-            }
-            if(!_.has(config, 'breadcrumbs')){
-                log.error('breadcrumbs configuration is not provided.');
-            }
-            if(!_.has(config, 'file_browser')){
-                log.error('file_browser configuration is not provided.');
-            }
-            if(!_.has(config, 'tab_controller.tool_palette')){
-                log.error('tool_palette configuration is not provided.');
-            }
-            if(!_.has(config, 'tab_controller')){
-                log.error('tab_controller configuration is not provided.');
-            }
-        },
+//        validateConfig: function(config){
+//            if(!_.has(config, 'services.workspace.endpoint')){
+//                throw 'config services.workspace.endpoint could not be found for remote log initialization.'
+//            } else {
+//                log.initAjaxAppender(_.get(config, 'services.workspace.endpoint'));
+//            }
+//            if(!_.has(config, 'breadcrumbs')){
+//                log.error('breadcrumbs configuration is not provided.');
+//            }
+//            if(!_.has(config, 'file_browser')){
+//                log.error('file_browser configuration is not provided.');
+//            }
+//            if(!_.has(config, 'tab_controller.tool_palette')){
+//                log.error('tool_palette configuration is not provided.');
+//            }
+//            if(!_.has(config, 'tab_controller')){
+//                log.error('tab_controller configuration is not provided.');
+//            }
+//        },
 
         render: function () {
-            log.debug("start: rendering breadcrumbs control");
-            this.breadcrumbController.render();
-            log.debug("end: rendering breadcrumbs control");
+            log.debug("start: rendering menu_bar control");
+            this.menuBar.render();
+            log.debug("end: rendering menu_bar control");
 
-            log.debug("start: rendering file_browser control");
-            this.fileBrowser.render();
-            log.debug("end: rendering file_browser control");
-
-            log.debug("start: rendering tab controller");
-            this.tabController.render();
-            log.debug("end: rendering tab controller");
-
-            var tab = this.tabController.newTab();
-            this.tabController.newTab();
+//            log.debug("start: rendering breadcrumbs control");
+//            this.breadcrumbController.render();
+//            log.debug("end: rendering breadcrumbs control");
+//
+//            log.debug("start: rendering file_browser control");
+//            this.fileBrowser.render();
+//            log.debug("end: rendering file_browser control");
+//
+//            log.debug("start: rendering tab controller");
+//            this.tabController.render();
+//            log.debug("end: rendering tab controller");
+//
+//            var tab = this.tabController.newTab();
+//            this.tabController.newTab();
         },
 
         applicationConstants: function() {
